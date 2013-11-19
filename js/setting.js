e4460a74505758b47398df505d48b5766a874817
takoratta <takoratta@gmail.com>
2013-11-19 22:13:04 +0900

    2nd batch fixes

--- js/setting.js
+++ js/setting.js
@@ -31,7 +31,8 @@ OpenSpending.Styles.Cofog = {
   '301': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' }, // 民生費 社会福祉費
   '302': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' }, // 民生費 児童福祉費
   '303': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' }, // 民生費 生活保護費
-  '304': { icon: 'icons/unemployment.svg', color: '#C75746', bcolor: '#4E6D00' }, // 民生費 国民年金費
+  '304': { icon: 'icons/unemployment.svg', color: '#C75746', bcolor: '#4E6D00' }, // 民生費 高齢者福祉費
+  '305': { icon: 'icons/unemployment.svg', color: '#C75746', bcolor: '#4E6D00' }, // 民生費 生活保護費
   '4': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#D33673' }, // 衛生費
   '401': { icon: 'icons/hospital.svg', color: '#C75746', bcolor: '#D33673' }, // 衛生費 保健衛生費
   '402': { icon: 'icons/medical-supplies.svg', color: '#C75746', bcolor: '#D33673' }, // 衛生費 清掃費
@@ -44,22 +45,26 @@ OpenSpending.Styles.Cofog = {
   '8': { icon: 'icons/coal.svg', color: '#C75746', bcolor: '#C75746' }, // 土木費
   '801': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' }, // 土木費 土木管理費
   '802': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#C75746' }, // 土木費 道路橋りょう費
-  '803': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' }, // 土木費 河川費
-  '804': { icon: 'icons/government-uk.svg', color: '#C75746', bcolor: '#C75746' }, // 土木費 都市計画費
-  '805': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#C75746' }, // 土木費 住宅費
+  '803': { icon: 'icons/government-uk.svg', color: '#C75746', bcolor: '#C75746' }, // 土木費 都市計画費
+  '804': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#C75746' }, // 土木費 住宅費
   '9': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#D33673' }, // 消防費
   '901': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#D33673' }, // 消防費 消防費
   '10': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#790586' }, // 教育費
   '1001': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 教育総労務費
-  '1002': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#790586' },  // 教育費 小学校費
-  '1003': { icon: 'icons/primary.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 中学校費
-  '1004': { icon: 'icons/books.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 社会教育費
-  '1005': { icon: 'icons/sports.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 保健体育費
+  '1002': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 教育総務費
+  '1003': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#790586' },  // 教育費 小学校費
+  '1004': { icon: 'icons/primary.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 中学校費
+  '1005': { icon: 'icons/primary.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 幼稚園費
+  '1006': { icon: 'icons/books.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 社会教育費
+  '1007': { icon: 'icons/sports.svg', color: '#C75746', bcolor: '#790586' }, // 教育費 社会体育費
   '11': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#D36B3B' }, // 公債費
   '1101': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#D36B3B' }, // 公債費
   '12': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#935B3B' }, // 諸支出金
-  '1201': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#935B3B' }, // 諸支出金 土地基金費
-  '1202': { icon: 'icons/petrol.svg', color: '#C75746', bcolor: '#935B3B' }, // 諸支出金 開発公社費
+  '1201': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#935B3B' }, // 諸支出金 公共施設管理基金費
+  '1202': { icon: 'icons/petrol.svg', color: '#C75746', bcolor: '#935B3B' }, // 諸支出金 財政調整基金費
+  '1203': { icon: 'icons/petrol.svg', color: '#C75746', bcolor: '#935B3B' }, // 諸支出金 公共施設整備基金費
+  '1204': { icon: 'icons/petrol.svg', color: '#C75746', bcolor: '#935B3B' }, // 諸支出金 市民活動推進基金費
+  '1205': { icon: 'icons/petrol.svg', color: '#C75746', bcolor: '#935B3B' }, // 諸支出金 環境基金費
   '13': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#C35B4B' }, // 予備費
   '1301': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#C35B4B' }  // 予備費 予備費
 };
